a97d9f795af7e4c477c432c6eb907f2c9c8cd5b0
ZJL2019 <379105244@qq.com>
2019-04-17 21:29:50 +0800

    change

--- test_4_16/test_4_16/test_4_16.c
+++ test_4_16/test_4_16/test_4_16.c
@@ -4,34 +4,47 @@
 #include<Windows.h>
 
 //third
-int main()
+//int (*arr)[20] 数组指针  int *arr[20]指针数组
+#define ROW 5
+void  Pascal(int(*arr)[ROW], int row, int col)
 {
-	int i, j, n;
-	int a[100][100] = { 0 };
-	do
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
 	{
-		printf("please enter a number:");
-		scanf("%d", &n);
-	} while (n<1 || n>100);
-	for (i = 0; i < n; i++)
-	{
-		a[i][0] = 1;
-	}
-	for (i = 1; i < n; i++)
-	{
-		for (j = 1; j <= i; j++)
+		for (j = 0; j < col; j++)
 		{
-			a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+			if (i == j || j == 0)
+			{
+				arr[i][j] = 1;
+			}
+			else
+			{
+				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+			}
 		}
 	}
-	for (i = 0; i < n; i++)
+}
+
+void Show(int(*arr)[ROW])
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < ROW; i++)
 	{
 		for (j = 0; j <= i; j++)
 		{
-			printf("%3d", a[i][j]);
+			printf("%-5d", arr[i][j]);
 		}
 		printf("\n");
 	}
+}
+
+int main()
+{
+	int arr[ROW][ROW] = { 0 };
+	Pascal(arr, ROW, ROW);
+	Show(arr);
 	system("pause");
 	return 0;
 }
@@ -42,9 +55,9 @@ int main()
 
 
 //second
-//int main()
+//void Killer()
 //{
-//	char killer=0;
+//	char killer = 0;
 //	for (killer = 'A'; killer <= 'D'; killer++)
 //	{
 //		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
@@ -52,6 +65,10 @@ int main()
 //			printf("killer is %c\n", killer);
 //		}
 //	}
+//}
+//int main()
+//{
+//	Killer();
 //	system("pause");
 //	return 0;
 //}
@@ -75,25 +92,15 @@ int main()
 //				{
 //					for (E = 1; E <= 5; E++)
 //					{
-//						if ((2 == B && 3 != A) || (2 != B && 3 == A))
+//						if ((2 == B) + (3 == A) == 1 &&
+//							(2 == B) + (4 == E) == 1 &&
+//							(1 == C) + (2 == D) == 1 &&
+//							(5 == C) + (3 == D) == 1 &&
+//							(4 == E) + (1 == A) == 1)
 //						{
-//							if ((2 == B && 4 != E) || (2 != B && 4 == E))
+//							if (A*B*C*D*E == 120)
 //							{
-//								if ((1 == C && 2 != D) || (1 != C && 2 == D))
-//								{
-//									if ((5 == C && 3 != D) || (5 != C && 3 == D))
-//									{
-//										if ((4 == E && 1 != A) || (4 != E && 1 == A))
-//										{
-//											if ((A != B) && (A != C) && (A != D) && (A != E) && (B != C) && \
-//												(B != D) && (B != E) && (C != D) && (C != E) && (D != E))
-//											{
-//												printf("A=%d\tB=%d\tC=%d\tD=%d\tE=%d\n", A, B, C, D, E);
-//											}
-//								
-//										}
-//									}
-//								}
+//								printf("A=%d\tB=%d\tC=%d\tD=%d\tE=%d\n", A, B, C, D, E);
 //							}
 //						}
 //					}
