4c771218327e59a5db781e80a2c4bf0deed832f9
ZJL2019 <379105244@qq.com>
2019-05-07 18:41:32 +0800

    test 实现strcpy函数

---
+++ test_5_7/test_5_7/test_5_7.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<Windows.h>
+#include<assert.h>
+char My_strcpy(char *a,char *b)// µœ÷strcpy
+{
+	assert(a != NULL);
+	assert(b != NULL);
+	char *p = a;
+	while ((*a++ = *b++) != '\0');
+	return p;
+}
+
+
+
+
+int main()
+{
+	char a[] = { "hello word!" };
+	char b[] = { "hello bit!" };
+	char ret=My_strcpy(a, b);
+	printf("%s\n", a);
+	system("pause");
+	return 0;
+}
